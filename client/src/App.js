3ce06d856beb8a3dc7e9fb8145196e5643d7c205
viclisa <v.climent.savall@gmail.com>
2019-03-11 23:05:47 +0100

    starting redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -8,21 +10,26 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
 import './App.css';
+import { createStore } from 'redux';
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className='App'>
-          <Navbar />
-          <Route exact path='/' component={Landing} />
-          <div className='container'>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <Route exact path='/' component={Landing} />
+            <div className='container'>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
