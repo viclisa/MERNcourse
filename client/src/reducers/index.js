b9f062f7ff6c821e73a65b5e16d2e4ce1a258ab4
viclisa <v.climent.savall@gmail.com>
2019-03-24 12:37:54 +0100

    Dashboard

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authreducer from './authReducer';
 import errorReducer from './errorReducer';
+import profileReducer from './profileReducer';
 
 export default combineReducers({
   auth: authreducer,
-  errors: errorReducer
+  errors: errorReducer,
+  profile: profileReducer
 });
